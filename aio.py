a80ac310f478fd2b9ff52a3b084b47516f3440cb
mence40 <56419133+mence40@users.noreply.github.com>
2020-12-17 16:33:57 -0800

    Update aio.py

--- aio.py
+++ aio.py
@@ -33,10 +33,7 @@ def lambda_handler(event=None, context=None):
             descrip_data = []
             path = dir + '/' + label
             for name in os.listdir(path):
-                image = cv2.imread(path + '/' + name)
-                keyp = orb.detect(image, None)
-                key, des = orb.compute(image, keyp)
-                descrip_data.append(des)
+                descrip_data.append(np.load(dir + '/' + label + '/' + name))
             features = np.zeros((len(descrip_data), 800), "float32")
             for i in range(len(descrip_data)):
                 words, dist = vq(descrip_data[i], vocab)
@@ -45,7 +42,7 @@ def lambda_handler(event=None, context=None):
             scaler = StandardScaler().fit(features)
             features = scaler.transform(features)
             if transfer_dom:
-                transformed_data = np.matmul(decorrelate(features), trg_transform)
+                transformed_data = np.matmul(decorrelate(features), trg_transform).astype(float)
                 data = np.vstack((data, transformed_data)) if len(data) else transformed_data
                 labels += [classification] * len(features)
             else:
@@ -53,8 +50,8 @@ def lambda_handler(event=None, context=None):
                 labels += [classification] * len(features)
         return data, labels
 
-    training_path = 'tmp/training/training'
-    valid_path = 'tmp/validation/validation'
+    training_path = '/tmp/training'
+    valid_path = '/tmp/validation'
 
     # Requires two arguments to work: label for the monkey species and k for k-shot learning
     target_species = 'n0'
@@ -68,14 +65,21 @@ def lambda_handler(event=None, context=None):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    logging.info("Hello World!")
-
     client = boto3.client('s3')
     resource = boto3.resource('s3')
 
-    download_dir(client, resource, "training/training/", '/tmp', bucket=bucketName)
-    download_dir(client, resource, "validation/validation/", '/tmp', bucket=bucketName)
+    download_dir(client, resource, "training/", '/tmp', bucket=bucketName)
+    download_dir(client, resource, "validation/", '/tmp', bucket=bucketName)
 
+    # for root, directories, filenames in os.walk('/tmp/'):
+    #     logging.info("made it in")
+    #     for directory in directories:
+    #         logging.info(os.path.join(root, directory))
+    #
+    #     for filename in filenames:
+    #         logging.info(os.path.join(root, filename))
+
+    #FUNCTION 1
     train_files = []
     for image in os.listdir(training_path + "/" + target_species):
         imgpath = training_path + "/" + target_species + "/" + image
@@ -84,16 +88,16 @@ def lambda_handler(event=None, context=None):
     random.shuffle(train_files)
     train_files = train_files[:k]
 
-    orb = cv2.ORB_create()
+    #orb = cv2.ORB_create()
     descrip = []
     for path in train_files:
-        image = cv2.imread(path)
-        keyp = orb.detect(image, None)
-        key, des = orb.compute(image, keyp)
-        descrip.append((path, des))
+        descrip.append(np.load(path))
 
-    descriptors = descrip[0][1]
-    for path, desc in descrip[1:]:
+    # Fixed
+    #logging.info(descrip[1:])
+
+    descriptors = descrip[0]
+    for desc in descrip[1:]:
         descriptors = np.vstack((descriptors, desc))
     descriptors_f = descriptors.astype(float)
 
@@ -101,7 +105,9 @@ def lambda_handler(event=None, context=None):
     vocab, var = kmeans(descriptors_f, words, 1)
     features = np.zeros((k, words), "float32")
     for i in range(k):
-        words, dist = vq(descrip[i][1], vocab)
+        # Fiexed
+        # logging.info(vq(descrip[i], vocab))
+        words, dist = vq(descrip[i], vocab)
         for w in words:
             features[i][w] += 1
     scaler = StandardScaler().fit(features)
@@ -110,8 +116,15 @@ def lambda_handler(event=None, context=None):
     # important
     trg_transform = gen_target_transform(features).astype(float)
 
+    #FUNCTION 2
+    #WHEN SEPARATED FROM 1, THESE NEED TO BE PASSED:
+    #1. TRG_TRANSFORM, JUST MAKE A NP DUMP LIKE "target_class_transform.npy" AND LOAD
+    #2. TARGET CLASS
+
     train_labels = [1] * len(features)
     train_data = features
+
+    assert (np.isfinite(train_data).all())
 
     # An 800 word vocabulary has been generated to represent the images. This vocabulary needs to be applied to the transofrmed data.
     # This should probably be the end of the first function.
@@ -122,15 +135,31 @@ def lambda_handler(event=None, context=None):
     train_labels += new_labels
     train_labels = np.array(train_labels)
     train_data = np.vstack((train_data, new_train_data))
+
+    assert (np.isfinite(train_data).all())
+
+    for line in train_data:
+        logging.info(line)
+
     train_data = train_data.astype(float)
+
+    assert (np.isfinite(train_data).all())
+
+    #FUNCTION 3
+    #WHEN SEPARATED FROM 2, THESE NEED TO BE PASSED:
+    #1. TARGET CLASS
+    #2. TRAIN_DATA, SHOULD BE SAVED AS NP DUMP IN 2
+    #3. TRAIN_LABELS
 
     # all data should now be transformed into the same domain. Last step is to load the test set and train a classifier
     valid_data, valid_labels = load(valid_path, target_species, vocab, include_target=True, transfer_dom=False)
     classifier = LinearSVC(max_iter=80000)
+    logging.info(train_data)
+    logging.info(train_labels)
     classifier.fit(train_data, train_labels)
     valid_predictions = classifier.predict(valid_data)
     print('RESULTS:, valid_acc=' + str(accuracy_score(valid_predictions, valid_labels)))
-
+    logging.info('RESULTS:, valid_acc=' + str(accuracy_score(valid_predictions, valid_labels)))
     print('done')
 
     return {
@@ -140,12 +169,12 @@ def lambda_handler(event=None, context=None):
 
 def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
+    # logger = logging.getLogger()
+    # logger.setLevel(logging.INFO)
+    #
     paginator = client.get_paginator('list_objects')
-
-    logging.info(paginator)
+    #
+    # logging.info(paginator)
     for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
 
         if result.get('CommonPrefixes') is not None:
