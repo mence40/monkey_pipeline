2038e3bb38a34b57bab908421698a2d43d5d74cd
mence40 <56419133+mence40@users.noreply.github.com>
2020-12-18 20:42:50 -0800

    Add files via upload

---
+++ Bash_Scripts/compBP2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# JSON object to pass to Lambda Function
+json={"\"shift\"":"\"22\"","\"msg\"":"\"ServerlessComputingWithFaaS\""}
+
+echo "Invoking Lambda funciton using AWS CLI"
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name CompBP2 --region us-east-1 --payload $json /dev/stdout | head -n 1 | head -c -2 ; echo`
+
+echo ""
+
+echo "JSON RESULT:"
+echo $output | jq
+echo ""
